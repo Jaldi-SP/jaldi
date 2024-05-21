d2a10c6e5b7c3253995b2dd5846af012ea9adf24
Sreyas Agarwal <sreyasa2@illinois.edu>
2024-05-21 18:44:23 +0530

    delete backend folder

--- backend/db/customer.js
+++
@@ -1,8 +0,0 @@
-const StatusEnum = {
-  WAITLIST: 'Waitlist',
-  SERVING: 'Serving',
-  COMPLETED: 'Completed',
-  INACTIVE: 'Inactive'  // New status for no visit today
-};
-
-module.exports =  StatusEnum;
