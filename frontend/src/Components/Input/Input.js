0e11495f84ff46db781d693d1eb4a6fb110c1e50
Sreyas Agarwal <sreyasa2@illinois.edu>
2024-05-29 19:32:19 +0530

    feat: handle enter on login page

--- frontend/src/Components/Input/Input.js
+++ frontend/src/Components/Input/Input.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './Input.scss';
+import React from "react";
+import "./Input.scss";
 
-const Input = ({ type="text", value, onChange, placeholder, prefix }) => {
-    return (
-        <div className="input-container">
-            {prefix && <span className="input-prefix">{prefix}</span>}
-            <input
-                className={`input ${prefix ? 'input-with-prefix' : ''}`}
-                type={type}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-            />
-        </div>
-    );
-}
+const Input = ({
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  prefix,
+  onKeyDown = () => {},
+}) => {
+  return (
+    <div className="input-container">
+      {prefix && <span className="input-prefix">{prefix}</span>}
+      <input
+        className={`input ${prefix ? "input-with-prefix" : ""}`}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        onKeyDown={onKeyDown}
+      />
+    </div>
+  );
+};
 
 export default Input;
--- frontend/src/Containers/Login/Login.js
+++ frontend/src/Containers/Login/Login.js
@@ -1,54 +1,65 @@
-import React, { useState } from 'react';
-import './Login.scss';
-import axios from 'axios';
-import Input from '../../Components/Input/Input';
-import Label from '../../Components/Label/Label';
-import ActionButton from '../../Components/ActionButton/ActionButton';
+import React, { useState } from "react";
+import "./Login.scss";
+import axios from "axios";
+import Input from "../../Components/Input/Input";
+import Label from "../../Components/Label/Label";
+import ActionButton from "../../Components/ActionButton/ActionButton";
 
 const Login = (props) => {
-    const {setAuthenticated} = props;
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const { setAuthenticated } = props;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-    const handleAccessCodeChange = (event) => {
-        setPassword(event.target.value);
-    };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const handleSubmit = async () => {
-        try {
-            const res = await axios.post("/auth/login", {username, password})
-            setAuthenticated(true);
-        } catch (err) {
-            console.log(err)
-        }
-        setUsername('');
-        setPassword('');
-    };
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("/auth/login", { username, password });
+      setAuthenticated(true);
+    } catch (err) {
+      console.log(err);
+    }
+    setUsername("");
+    setPassword("");
+  };
 
-    return (
-        <div className='login-container'>
-            <div className="login-form">
-                <Label text="Username"/>
-                <Input
-                    value={username}
-                    onChange={handleUsernameChange}
-                    placeholder={"Username"}
-                />
-                <Label text="Password"/>
-                <Input
-                    type="password"
-                    value={password}
-                    onChange={handleAccessCodeChange}
-                    placeholder={"Access code"}
-                />
-                <ActionButton label="Sign in" id="sign-in-button" onClick={handleSubmit}/>
-            </div>
-        </div>
-    );
-}
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <Label text="Username" />
+        <Input
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder={"Username"}
+        />
+        <Label text="Password" />
+        <Input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          onKeyDown={handleKeyPress}
+          placeholder={"Password"}
+        />
+        <ActionButton
+          label="Sign in"
+          id="sign-in-button"
+          onClick={handleSubmit}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Login;
