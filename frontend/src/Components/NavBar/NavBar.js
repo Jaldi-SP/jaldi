84e8887f7335ca7c1b75cb6dfeb032b0360cc30a
Prakarsh Gupta <pg12@illinois.edu>
2024-06-17 20:55:14 -0700

    added mobile menu icon

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import "./NavBar.scss";
 import Icon from "../Icon/Icon";
 import axios from "axios";
+import { useState } from "react";
 
 const NavBar = ({
     setAuthenticated,
@@ -9,8 +10,9 @@ const NavBar = ({
     setShowCompleted,
     setShowInvalid,
     setShowSetting,
-    setShowCustomerDirectory
+    setShowCustomerDirectory,
 }) => {
+    const [showMenu, setShowMenu] = useState(false);
     const logout = async () => {
         try {
             const res = axios.post(`/auth/logout`);
@@ -82,6 +84,7 @@ const NavBar = ({
                 <button
                     title="Directory"
                     className="icon"
+                    id="directory-icon"
                     onClick={() => {
                         setShowWaitlist(false);
                         setShowServing(false);
@@ -96,6 +99,7 @@ const NavBar = ({
                 <button
                     title="Settings"
                     className="icon"
+                    id="settings-icon"
                     onClick={() => {
                         setShowWaitlist(false);
                         setShowServing(false);
@@ -107,14 +111,91 @@ const NavBar = ({
                 >
                     <Icon name="settings" size={30} color="#13293dff" />
                 </button>
+                <button
+                    title="Menu"
+                    className="icon"
+                    id="menu-icon"
+                    onClick={() => {
+                        setShowMenu(true);
+                        setShowWaitlist(false);
+                        setShowServing(false);
+                        setShowCompleted(false);
+                        setShowInvalid(false);
+                        setShowSetting(false);
+                        setShowCustomerDirectory(false);
+                    }}
+                >
+                    <Icon name="menu" size={30} color="#13293dff" />
+                </button>
             </div>
             <button
                 title="Logout"
                 className="icon logout-icon"
+                id="logout-icon"
                 onClick={logout}
             >
                 <Icon name="log-out" size={30} color="#FF0000" />
             </button>
+            {showMenu && (
+                <div className="mobile-menu">
+                    <button
+                        id="close-menu-icon"
+                        onClick={() => {
+                            setShowMenu(false);
+                            setShowWaitlist(true);
+                            setShowServing(false);
+                            setShowCompleted(false);
+                            setShowInvalid(false);
+                            setShowSetting(false);
+                            setShowCustomerDirectory(false);
+                        }}
+                    >
+                        <Icon name="x" size={30} color="red" />
+                    </button>
+                    <div className="button-list">
+                        <div
+                            className="button-list-item"
+                            onClick={() => {
+                                setShowWaitlist(false);
+                                setShowServing(false);
+                                setShowCompleted(false);
+                                setShowInvalid(false);
+                                setShowSetting(false);
+                                setShowCustomerDirectory(true);
+                                setShowMenu(false);
+                            }}
+                        >
+                            <button>
+                                <Icon name="user" size={30} color="white" />
+                            </button>
+                            <p id="button-list-item-text">Customer Directory</p>
+                        </div>
+                        <div
+                            className="button-list-item"
+                            onClick={() => {
+                                setShowWaitlist(false);
+                                setShowServing(false);
+                                setShowCompleted(false);
+                                setShowInvalid(false);
+                                setShowSetting(true);
+                                setShowCustomerDirectory(false);
+                                setShowMenu(false);
+                            }}
+                        >
+                            <button>
+                                <Icon name="settings" size={30} color="white" />
+                            </button>
+                            <p id="button-list-item-text">Settings</p>
+                        </div>
+                        <div className="button-list-item" onClick={logout}>
+                            <button>
+                                <Icon name="log-out" size={30} color="white" />
+                            </button>
+                            <p id="button-list-item-text">Logout</p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
